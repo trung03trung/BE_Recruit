8b9fa764f14b3e406bb941dad211c5c903d25829
trung03trung <89577344+trung03trung@users.noreply.github.com>
2022-07-20 09:34:52 +0700

    update profile

--- src/main/java/com/itsol/recruit/service/ProfileService.java
+++ src/main/java/com/itsol/recruit/service/ProfileService.java
@@ -1,2 +1,16 @@
-package com.itsol.recruit.service;public class ProfileService {
+package com.itsol.recruit.service;
+
+import com.itsol.recruit.entity.AcademicLevel;
+import com.itsol.recruit.repository.AcademicLevelRepository;
+
+import java.util.List;
+
+public class ProfileService {
+    private final AcademicLevelRepository academicLevelRepository;
+
+    public ProfileService(AcademicLevelRepository academicLevelRepository) {
+        this.academicLevelRepository = academicLevelRepository;
+    }
+
+//    public List<AcademicLevel> getAll(){}
 }
