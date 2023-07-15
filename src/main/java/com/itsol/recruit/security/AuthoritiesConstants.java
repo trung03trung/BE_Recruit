6c137a629e45c6469b2ed7a7a06ae57545b740ba
unknown <trung03trung@gmail.com>
2023-07-15 19:35:49 +0700

    up

--- src/main/java/com/itsol/recruit/security/AuthoritiesConstants.java
+++ src/main/java/com/itsol/recruit/security/AuthoritiesConstants.java
@@ -5,4 +5,6 @@ public class AuthoritiesConstants {
     public static final String ROLE_USER = "ROLE_USER";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
 
+    public static final String ROLE_JE = "ROLE_JE";
+
 }
