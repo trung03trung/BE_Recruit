3bb7eb99f070960a33e375f2d329038c6d1c84cf
trung03trung <trung03trung@gmail.com>
2023-08-19 15:31:57 +0700

    update

--- src/main/java/com/itsol/recruit/dto/TextBookDTO.java
+++ src/main/java/com/itsol/recruit/dto/TextBookDTO.java
@@ -3,13 +3,15 @@ package com.itsol.recruit.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
-public class TextBookDTO {
+@NoArgsConstructor
+public class TextBookDTO extends BaseObj {
     String id;
 
     String name;
